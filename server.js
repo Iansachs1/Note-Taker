9100bf6e66e177d4c875aafc93b55d6b2f65b93a
Iansachs1 <iansachs1@gmail.com>
2020-09-22 21:59:41 -0700

    added comments for clarity

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-const { response } = require("express");
+// all require statemente
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
+// references the json file containing my notes api info
 var dbNotes = require("./db/db.json");
 
+// connection setup qtuff for express
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// variable used to get the path to this page
 const scriptPath = __dirname;
 
+// middleware for express including the static folder which took me forever to find i was missing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public')); // Allows static files in public folder to be accessed
 
+//=========================================================================================
+// functions to affect the json database
+//=========================================================================================
+
+// reads all contets from the json database and returns it to the page
 function readFromDB (response) {
     const filePath = path.join(scriptPath, "db", "./db.json");
     fs.readFile(filePath, { encoding: "utf8" }, function (error, fileContent) {
@@ -22,6 +31,8 @@ function readFromDB (response) {
     });
 }
 
+
+// takes in a new note and pushes it to the json database
 function writeToDB (newNote) {
     const filePath = path.join(scriptPath, "db", "db.json");
 
@@ -34,6 +45,7 @@ function writeToDB (newNote) {
     });
 }
 
+// finds the array index of the not from the delete request and removes it from the array
 function deleteFromDB (deletedNote) {
     const filePath = path.join(scriptPath, "db", "db.json");
     dbNotes.splice((deletedNote - 1), 1);
@@ -45,6 +57,7 @@ function deleteFromDB (deletedNote) {
     });
 }
 
+// creates an id for all notes in the database
 function generateID(dbNotes) {
     for (let i = 0; i < dbNotes.length; i++) {
         dbNotes[i].id = (i + 1);
@@ -52,16 +65,25 @@ function generateID(dbNotes) {
 }
 
 
+
+//=========================================================================================
+// routes and listeners
+//=========================================================================================
+
+
+// displays the root route
 app.get('/', function (request, response) {
     const filePath = path.join(scriptPath, "public", "./index.html");
     response.sendFile(filePath, "index.html");
 });
 
+// displays the /notes route
 app.get('/notes', function (request, response) {
     const filePath = path.join(scriptPath, "public", "./notes.html");
     response.sendFile(filePath, "notes.html");
 });
 
+// displays the /api/notes route
 app.get('/api/notes', function (request, response) {
     readFromDB(response);
 });
@@ -71,25 +93,27 @@ app.get("/api/notes/:id", function (request, response) {
   
     for (var i = 0; i < dbNotes.length; i++) {
       if (chosen === dbNotes[i].id) {
-        return response.json(dbNotes[i +1]);
+        return response.json(dbNotes[i + 1]);
       }
     }
   
     return response.json(false);
 });
 
+// writes the note from the post request to the database
 app.post("/api/notes", function (request, response) {
     let newNote = request.body;
     response.send(writeToDB(newNote));
 });
 
+// calls the deletefromDB function and passes the id of the note from the delete request to the function
 app.delete('/api/notes/:id', function (request, response) {
     var deletedNote = request.params.id
     deleteFromDB(deletedNote);
     response.status(204);
 });
 
-
+// bog standard listener. jedi business, go back to your drinks
 app.listen(PORT, function() {
     console.log('listening on port ' + PORT);
 });
